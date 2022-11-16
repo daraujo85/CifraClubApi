b0cc2858ecf124aedb6bf3638cfb242f7b7491c0
Diego Araujo <diegoaraujo@MacBook-Air-de-Diego-2.local>
2022-11-16 03:20:31 -0300

    Detalhamento do resutlado da busca

--- app.js
+++ app.js
@@ -3,51 +3,73 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-    (async () => {
-        app.get('/findCifraClub/:title', async (req, res) => {
-            
-            const cifraClubResults = await findCifraClub(req.params.title);
+(async () => {
+    app.get('/findCifraClub/:title', async (req, res) => {
 
-            res.send(cifraClubResults)
-        })
+        const cifraClubResults = await findCifraClub(req.params.title);
 
-        app.listen(port, () => {
-            console.log("NodeAPI listening on port:",  port)
-        })
+        res.send(cifraClubResults)
+    })
 
-        
-    })();
+    app.listen(port, () => {
+        console.log("NodeAPI listening on port:", port)
+    })
+
+
+})();
 
 async function findCifraClub(title) {
 
     const browser = await puppeteer.launch({ headless: true, timeout: 60000 });
-        const page = await browser.newPage();
+    const page = await browser.newPage();
 
-        await page.setDefaultNavigationTimeout(0); 
-        
-        const url = 'https://www.cifraclub.com.br/?q=' + title;
+    await page.setDefaultNavigationTimeout(0);
 
-        console.log("URL da busca:", url);
+    const url = 'https://www.cifraclub.com.br/?q=' + title;
 
-        await page.goto(url);
+    console.log("URL da busca:", url);
 
-        var cifraClubResults = await page.evaluate(() =>
-            Array.from(
-                document.querySelectorAll('a.gs-title'),
-                function (element) {
-                    return { description: element.textContent, link: element.getAttribute('href') }
-                }
-            )
-        );
+    await page.goto(url);
 
-        cifraClubResults = uniqByKeepFirst(cifraClubResults, x => x.description).filter(x => x.link != null);
+    var cifraClubResults = await page.evaluate(() =>
+        Array.from(
+            document.querySelectorAll('a.gs-title'),
+            function (element) {
 
-        console.log(cifraClubResults)
-        //await page.screenshot({path: 'example.png'});
-        await browser.close();
+                const replaceTerms = [" - CIFRA CLUB", " - Cifra Club", " - Cifra ..."];
+                var descriptionValue = element.textContent;
+                
+                replaceTerms.forEach(element => {
+                    descriptionValue = descriptionValue.replace(element, "").trim();
+                });
+                
+                var parts = descriptionValue.split("-");
+                var titleValue = parts[0]; 
+                var artistValue = parts[1];
 
-        return cifraClubResults;
-}   
+                return { title: titleValue, artist: artistValue, link: element.getAttribute('href') }
+            }
+        )
+    );
+
+    cifraClubResults = uniqByKeepFirst(cifraClubResults, x => x.link).filter(x => x.link != null);
+
+    cifraClubResults = formartResult(cifraClubResults);
+
+    console.log(cifraClubResults)
+    //await page.screenshot({path: 'example.png'});
+    await browser.close();
+
+    return cifraClubResults;
+}
+function formartResult(items) {
+    items.forEach(element => {
+        element.title = element.title.trim();
+        element.artist = element.artist.trim();
+    });
+
+    return items;
+}
 function uniqByKeepFirst(a, key) {
     let seen = new Set();
     return a.filter(item => {
